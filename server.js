b3604875c1412cd0f4b2e794129a2470d5866cb9
bethjm <b.jeanne.mills@gmail.com>
2024-01-19 14:29:05 -0500

    delete route working correctly

--- server.js
+++ server.js
@@ -7,6 +7,7 @@ app.use(express.json());
 
 //get all tasks
 //WORKS SUCCESSFULLY, TESTED ON POSTMAN
+//DOUBLE CHECKED IS WORKING 2:06
 app.get("/api/tasks", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM tasks");
@@ -19,15 +20,16 @@ app.get("/api/tasks", async (req, res) => {
 });
 
 //get ind task
-//WORKS SUCCESSFULLY, TESTED ON POSTMAN
+//NOT WORKING 2:06
+//WORKING 2:09 - needed to take out task_id
 app.get("/api/tasks/:id", async (req, res) => {
   console.log("el id", req.params.id);
 
   try {
-    const results = await db.query("SELECT* FROM tasks WHERE task_id = $1", [
+    const results = await db.query("SELECT* FROM tasks WHERE id = $1", [
       req.params.id,
-    ]); //should this be id or task id? -pretty sure id. did console.log test and task_id comes up undefined
-    res.status(201).json(results.id);
+    ]);
+    res.status(201).json(results);
   } catch (error) {
     console.error("Error executing query", error);
     res.status(500).send("Internal Server Error");
@@ -36,6 +38,7 @@ app.get("/api/tasks/:id", async (req, res) => {
 
 //create a task
 //WORKS SUCCESSFULLY, TESTED ON POSTMAN
+//DOUBLE CHECKED IS WORKING 2:06
 app.post("/api/tasks", async (req, res) => {
   console.log(req.body);
 
@@ -52,11 +55,11 @@ app.post("/api/tasks", async (req, res) => {
   }
 });
 
-// update
+//update
 //successfully pulling data from here to postman, but not seeing anything different in my postgres table
 //I am getting updates to my get all request in postman though
 //pause for now and come back to later
-//I dont think my thing is properly hooked up to postgreqsl?
+//Is this hooked up properly to my local postgres?
 app.put("/api/tasks/:id", async (req, res) => {
   try {
     const results = await db.query(
@@ -83,11 +86,19 @@ app.put("/api/tasks/:id", async (req, res) => {
   console.log(req.body);
 });
 
-// delete app.delete("/api/tasks/:id", (req, res) => {
-//   res.status(204).json({
-//     status: "success",
-//   });
-// });
+//delete
+//test on postman, is returning proper values in terminal after being delted
+delete app.delete("/api/tasks/:id", async (req, res) => {
+  try {
+    const results = await db.query("DELETE FROM tasks WHERE id = $1", [
+      req.params.id,
+    ]);
+
+    res.status(201).json();
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 const port = process.env.PORT || 3001;
 app.listen(port, () => {
