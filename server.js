71a2b31192a36c6b8a5a94bf872651eb16500e7b
bethjm <b.jeanne.mills@gmail.com>
2024-01-19 10:31:28 -0500

    get ind tasks working successfully with postgres

--- server.js
+++ server.js
@@ -6,10 +6,11 @@ const app = express();
 app.use(express.json());
 
 //get all tasks
+//WORKS SUCCESSFULLY
 app.get("/api/tasks", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM tasks");
-    res.json(result);
+    res.status(200).json(result);
     console.log("Ye route be handled successfully", result);
   } catch (error) {
     console.error("Error executing query", error);
@@ -18,20 +19,19 @@ app.get("/api/tasks", async (req, res) => {
 });
 
 //get ind task
-app.get("/api/tasks/:id", (req, res) => {
-  console.log(req.params);
+//WORKS SUCCESSFULLY
+app.get("/api/tasks/:id", async (req, res) => {
+  console.log("el id", req.params.id);
 
-  res.status(200).json({
-    status: "success",
-    data: {
-      task_id: 1,
-      name: "Sample Task",
-      description: "Description of the task",
-      completed: false,
-      due_date: "2024-02-01",
-      urgency: "High",
-    },
-  });
+  try {
+    const results = await db.query("SELECT* FROM tasks WHERE task_id = $1", [
+      req.params.id,
+    ]); //should this be id or task id? -pretty sure id. did console.log test and task_id comes up undefined
+    res.status(201).json(results.id);
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 //create a task
