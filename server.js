90212a0ddbde44731ec1304b43e1be458113379b
bethjm <b.jeanne.mills@gmail.com>
2024-01-18 19:20:43 -0500

    created dummy CRUD routes and successfully tested on postman

--- server.js
+++ server.js
@@ -1,11 +1,63 @@
 require("dotenv").config();
-
 const express = require("express");
-
 const app = express();
 
-const port = process.env.PORT || 3001;
+app.use(express.json());
 
+//get all tasks
+app.get("/api/tasks", (req, res) => {
+  console.log("route handler ran");
+  res.status(200).json({
+    status: "success",
+    data: {
+      list_item: ["clean cat box", "eat", "play with cats"],
+    },
+  });
+});
+
+//get ind task
+app.get("/api/tasks/:id", (req, res) => {
+  console.log(req.params);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      list_item: ["drink water", "eat food", "play with dogs"],
+    },
+  });
+});
+
+//create a task
+app.post("/api/tasks", (req, res) => {
+  console.log(req.body);
+  res.status(201).json({
+    status: "success",
+    data: {
+      list_item: ["drink water", "eat food", "play with dogs"],
+    },
+  });
+});
+
+// update
+app.put("/api/tasks/:id", (req, res) => {
+  console.log(req.params.id);
+  console.log(req.body);
+  res.status(200).json({
+    status: "success",
+    data: {
+      list_item: ["drink water", "eat food", "play with dogs"],
+    },
+  });
+});
+
+// delete
+app.delete("/api/tasks/:id", (req, res) => {
+  res.status(204).json({
+    status: "success",
+  });
+});
+
+const port = process.env.PORT || 3001;
 app.listen(port, () => {
-  console.log("ye server be up and listening on port", port);
+  console.log("Yer server be afloat and keenly listenin' on the port", port);
 });
