c5067a086e719016475609ae7bfeb6894d80712d
bethjm <b.jeanne.mills@gmail.com>
2024-01-19 10:43:48 -0500

    successfully created create route

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const app = express();
 app.use(express.json());
 
 //get all tasks
-//WORKS SUCCESSFULLY
+//WORKS SUCCESSFULLY, TESTED ON POSTMAN
 app.get("/api/tasks", async (req, res) => {
   try {
     const result = await db.query("SELECT * FROM tasks");
@@ -19,7 +19,7 @@ app.get("/api/tasks", async (req, res) => {
 });
 
 //get ind task
-//WORKS SUCCESSFULLY
+//WORKS SUCCESSFULLY, TESTED ON POSTMAN
 app.get("/api/tasks/:id", async (req, res) => {
   console.log("el id", req.params.id);
 
@@ -35,19 +35,20 @@ app.get("/api/tasks/:id", async (req, res) => {
 });
 
 //create a task
-app.post("/api/tasks", (req, res) => {
+app.post("/api/tasks", async (req, res) => {
   console.log(req.body);
-  res.status(201).json({
-    status: "success",
-    data: {
-      task_id: 1,
-      name: "Sample Task",
-      description: "Description of the task",
-      completed: false,
-      due_date: "2024-02-01",
-      urgency: "High",
-    },
-  });
+
+  try {
+    const results = await db.query(
+      "INSERT INTO tasks (name, description, due_date, urgency) VALUES ($1, $2,$3, $4) RETURNING *",
+      [req.body.name, req.body.description, req.body.due_date, req.body.urgency]
+    );
+    console.log(results);
+    res.status(201).json(req.body);
+  } catch (error) {
+    console.error("Error executing query", error);
+    res.status(500).send("Internal Server Error");
+  }
 });
 
 // update
